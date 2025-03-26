68c5a192439671b58232a5663df1b0c2332ed4d2
Eneko Yranzo <eneko.yranzo@ikasle.egibide.org>
2025-03-26 20:20:17 +0100

    Ventana de bienvenida

---
+++ src/main/java/Vista/VBienvenida.java
@@ -0,0 +1,12 @@
+package Vista;
+
+import javax.swing.*;
+
+public class VBienvenida {
+    private JPanel pPrincipal;
+    private JLabel LImageLogo;
+    private JButton accesoABancaOnlineButton;
+    private JPanel pArriba;
+    private JPanel pAnuncio;
+    private JLabel LAnuncio;
+}
